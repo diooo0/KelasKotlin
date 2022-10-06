f97689a0e27880166a2c608065fd89d644ee3252
Egi <egidioegios@gmail.com>
2022-10-06 10:39:38 +0700

    .

--- src/main/Kotlin/Latihan.kt
+++ src/main/Kotlin/Latihan.kt
@@ -1,14 +1,11 @@
 fun main() {
     var lamaParkir: Int
     var biayaParkir: Float
-
     lamaParkir = 8
-
     //aturan bayar parkir
     val tarifDasar = 1.0
     val tarifBerikutnya = 0.5
     val maxTarifPerHari = 15
-
     if (lamaParkir <= 5) {
         biayaParkir = (lamaParkir * tarifDasar).toFloat()
     } else if ((lamaParkir > 5) && (lamaParkir < 24)) {
@@ -16,7 +13,6 @@ fun main() {
     } else {
         biayaParkir = maxTarifPerHari.toFloat()
     }
-
     println("Lama Parkir: $lamaParkir")
     println("Biaya Parkir: $biayaParkir")
 }
